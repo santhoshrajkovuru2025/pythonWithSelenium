785f9b54b497287b7d2f1eced22b1ac547bde7db
santhoshrajkovuru2025 <srkovuru@outlook.com>
2025-06-04 05:40:25 +0530

    Added new code

--- Basics.py
+++ Basics.py
@@ -8,3 +8,17 @@ print('Hello')
 
 a=3
 print(a)
+
+str = "Neethu"
+print(str)
+
+b , c, d, e = 5, 6.4, "Great",True
+f = 10+3j
+print(f)
+
+print("{} {}".format("value is", b))
+
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
---
+++ List.py
@@ -0,0 +1,10 @@
+List = [1, "Neethu", 5, 7, 9]
+
+print(List[0]) , # List is a data type, that can have multiple data types
+print(List[-1]) , #-1 is used to print last index
+print(List[0:4]), # is used to print the values from 0 to 4 values at a time
+
+# to insert a command in the List
+
+List.insert(2, "Sharma")
+print(List)
