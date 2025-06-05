46f8a31249f1fa7b8e33c22bd3a76c95161f3187
santhoshrajkovuru2025 <srkovuru@outlook.com>
2025-06-05 06:36:37 +0530

    Added fucntion code

--- Python Basics/function_Group.py
+++ Python Basics/function_Group.py
@@ -1,2 +1,15 @@
 # In python , function is used to perform the specific tasks
 
+# Function declaration
+
+def GreetMe(name):
+    print("Good Morning" + name)
+    #First Call
+
+GreetMe("Santhosh Raj Kovuru")
+
+def AddIntegers(a,b):
+    return a+b
+
+AddIntegers(2,5)
+print(AddIntegers(4,5))
